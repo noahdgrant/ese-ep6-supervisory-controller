e742807e9c85fc2a6edfa98dcfdc7cad02fac1e8
Noah Grant <noahdgrant@gmail.com>
2024-07-05 14:29:28 -0400

    Rebase with main

--- src/supervisory_controller.cpp
+++ src/supervisory_controller.cpp
@@ -9,18 +9,6 @@
 
 using namespace std;
 
-static void QueuePrint(deque<uint8_t>& queue) {
-    printf("[QUEUE]");
-    if (!queue.empty()) {
-        for (auto item : queue) {
-            printf(" %d", item);
-        }
-        printf("\n");
-    } else {
-        printf(" is empty\n");
-    }
-}
-
 static void QueueAdd(deque<uint8_t>& queue, int current_floor, int new_floor) {
     // NOTE: Only add floors to the queue that are not already in the queue.
     // If a floor is already in the queue, then it doesn't need to be added
@@ -43,8 +31,6 @@ static void QueueAdd(deque<uint8_t>& queue, int current_floor, int new_floor) {
     }
 }
 
-using namespace std;
-
 static void QueuePrint(deque<uint8_t>& queue) {
     printf("[QUEUE]");
     if (!queue.empty()) {
@@ -54,28 +40,6 @@ static void QueuePrint(deque<uint8_t>& queue) {
         printf("\n");
     } else {
         printf(" is empty\n");
-    }
-}
-
-static void QueueAdd(deque<uint8_t>& queue, int current_floor, int new_floor) {
-    // NOTE: Only add floors to the queue that are not already in the queue.
-    // If a floor is already in the queue, then it doesn't need to be added
-    // again since everyone waiting for the floor can get in the elevator
-    // at the same time.
-    if (find(queue.begin(), queue.end(), new_floor) == queue.end()) {
-        if (queue.empty()) {
-            queue.push_back(new_floor);
-        } else {
-            if (current_floor < new_floor) {
-                // Elevator is moving up
-                auto it = find_if(queue.begin(), queue.end(), [&](int floor) { return floor > new_floor; });
-                queue.insert(it, new_floor);
-            } else {
-                // Elevator is moving down
-                auto it = find_if(queue.rbegin(), queue.rend(), [&](int floor) { return floor < new_floor; });
-                queue.insert(it.base(), new_floor);
-            }
-        }
     }
 }
 
@@ -121,7 +85,7 @@ void SupervisoryController::run() {
                         break;
                 }
 
-                m_database.insert_floor_history(floor_number);
+                m_database.update_floor_history(floor_number);
                 last_floor = current_floor;
                 waiting = false;
             }
@@ -145,7 +109,7 @@ void SupervisoryController::run() {
 
             floor_number = msg.DATA[0] - 4; // NOTE: -4 converts to true floor number
 
-            m_database.insert_request_history(request_method, floor_number);
+            m_database.update_request_history(request_method, floor_number);
             QueueAdd(request_queue, current_floor - 4, floor_number); // NOTE: -4 converts to true floor number
             QueuePrint(request_queue);
         }
