5fab9e5bab75959abd1a2518693e38acd6885982
Noah Grant <noahdgrant@gmail.com>
2024-07-05 14:12:42 -0400

    Values added to request queue are sorted

--- src/supervisory_controller.cpp
+++ src/supervisory_controller.cpp
@@ -45,7 +45,7 @@ static void QueueAdd(deque<uint8_t>& queue, int current_floor, int new_floor) {
 
 using namespace std;
 
-static void print_queue(deque<uint8_t> queue) {
+static void QueuePrint(deque<uint8_t>& queue) {
     printf("[QUEUE]");
     if (!queue.empty()) {
         for (auto item : queue) {
@@ -54,6 +54,28 @@ static void print_queue(deque<uint8_t> queue) {
         printf("\n");
     } else {
         printf(" is empty\n");
+    }
+}
+
+static void QueueAdd(deque<uint8_t>& queue, int current_floor, int new_floor) {
+    // NOTE: Only add floors to the queue that are not already in the queue.
+    // If a floor is already in the queue, then it doesn't need to be added
+    // again since everyone waiting for the floor can get in the elevator
+    // at the same time.
+    if (find(queue.begin(), queue.end(), new_floor) == queue.end()) {
+        if (queue.empty()) {
+            queue.push_back(new_floor);
+        } else {
+            if (current_floor < new_floor) {
+                // Elevator is moving up
+                auto it = find_if(queue.begin(), queue.end(), [&](int floor) { return floor > new_floor; });
+                queue.insert(it, new_floor);
+            } else {
+                // Elevator is moving down
+                auto it = find_if(queue.rbegin(), queue.rend(), [&](int floor) { return floor < new_floor; });
+                queue.insert(it.base(), new_floor);
+            }
+        }
     }
 }
 
